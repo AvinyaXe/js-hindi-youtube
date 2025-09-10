73812f8ee3112b2a7577827e4d2228bc62967070
Vansh Gill <148219796+AvinyaXe@users.noreply.github.com>
2025-09-10 15:27:02 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = "23adw"
+// let score = null
+// let score = undefined
+// let score = false
+let score = "Vansh"
+
+
+// const {score} = req.body
+
+// "33" => 33
+// "33adw" => NaN
+// null => 0
+// undefined => NaN
+// "Vansh" => NaN
+// true => 1 | false => 0
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// let isLoggedIn = 1
+let isLoggedIn = "Vansh"
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(typeof isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true | 0 => false
+// "" => false | "Vansh" => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
