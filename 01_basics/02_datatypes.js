f448beef60b361dcf87f9a1a0a1eead79f321148
Vansh Gill <148219796+AvinyaXe@users.noreply.github.com>
2025-09-10 13:13:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(2 
+    + 
+    3) // code readability is the priority(or should be high)
+
+console.log("Vansh")
+
+
+let name = "Vansh"
+let age = 20
+let isLoggedIn = true
+let state;
+
+// number => 2^53 (range)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => abhi value asign nhai hui hai
+// symbol => uniqueness
+
+// object
+
+console.log(typeof "Vansh")
+console.log(typeof null) // type => object
+console.log(typeof undefined) // type => undefined
