704c2dfe94990e5ae897d6f3c0e3d7177a06a3a8
Vansh Gill <148219796+AvinyaXe@users.noreply.github.com>
2025-09-12 16:51:48 +0000

    datatypes recap

---
+++ datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non primitive)
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 572938475298765943872n
+
+// Array, Objects, Funtions
